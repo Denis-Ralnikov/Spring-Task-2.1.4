a4224f0aee39814ba49b9bd363181f7e9b3871f3
Tanservill <dentreyt2909@gmail.com>
2023-07-24 11:25:50 +0300

    Task complete

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    public String toString(){
+        return "\nВот и сказочке конец, а кто слушал молодец";
+    }
 }
